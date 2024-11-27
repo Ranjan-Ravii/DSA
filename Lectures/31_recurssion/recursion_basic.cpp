b0a1766d9b8541f9e6a4dd26fa31c5824c2b8050
Ravi Ranjan <inboxraviiranjan@gmail.com>
2024-11-27 23:55:35 +0530

    recursion Day 1

--- Lectures/31_recurssion/recursion_basic.cpp
+++ Lectures/31_recurssion/recursion_basic.cpp
@@ -1,18 +1,62 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int factorial(int n){
 
-    if (n == 0)
-        return 1;
+    //base case ==>> base is always required,it specifies when to terminate the callback.
 
-    int ans = 1;
-    ans = n * factorial(n - 1);
+    if (n == 0){
+        return 1;
+    }
+
+    int ans = n * factorial(n - 1);
     return ans;
 }
 
+int power(int base, int n){
+    if (n == 0)
+    {
+        return 1;
+    }
+    
+    int ans = base * pow(base, n-1);
+
+    return ans;
+}
+
+// printing count 
+void count(int num){
+
+    if(num == 0){
+        return ;
+    }
+
+    // printing in increasing order
+    count(num - 1 );
+    cout << num << endl;
+
+    /*
+    // printing in decreasing order;
+    cout << num << endl;
+    count(num - 1 );
+    */
+}
+
+
 int main(){
     int num = 6;
-    int ans = factorial(num);
-    cout << ans << endl;
+    int fact = factorial(num);
+    cout << "factorial of "<< num << " is : " << fact << endl;
+
+    int pow = power(num,2);
+    cout << "square of "<< num << " is : " << pow << endl;
+
+
+// printing count
+    cout << "Enter the number : ";
+    cin >> num;
+    count(num);
+
+
 }
